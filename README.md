c7a9b5b317ae585f85badd4c15ec0bc50fb90d0b
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2022-11-23 22:42:19 +0000

    Finished tempConvert.cpp program and added comments.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Unit5-01-CPP
+[![Mr Coxall's Super Linter](https://github.com/ICS3U-Programming-VanN/Unit5-01-CPP/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/ICS3U-Programming-VanN/Unit5-01-CPP/actions/)
+
---
+++ tempConvert.cpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2022 Van Nguyen All rights reserved.
+//
+// Created by: Van Nguyen
+// Created on: November 22, 2022
+// This program asks the user for the temperature
+// in celsius and then it converts it to fahrenheit
+// The program will then display the converted temperature to the user
+
+
+#include <iostream>
+
+
+// This function converts asks the user for the temperature in celsius,
+// then converts it to fahrenheit and then displays it to the user
+void fahrenheit() {
+    // Declared Variables
+    float celsiusFloat, fahrenheit;
+    std::string celsiusString;
+
+    // Asks user for the temperature in celsius
+    std::cout << "Enter the temperature in degrees celsius (°C): ";
+    std::cin >> celsiusString;
+
+    // Checks for exceptions
+    try {
+        // Converts the celsius variable to a float
+        celsiusFloat = stof(celsiusString);
+
+        // Converts (calculates) the celsius to fahrenheit
+        fahrenheit = (static_cast<float>(9) / static_cast<float>(5))
+        * celsiusFloat + 32;
+
+        // Displays the conversion to the user (including the fahrenheit)
+        std::cout << celsiusFloat << "°C is equal to " << fahrenheit << "°F"
+        << std::endl;
+
+    // In the event of an exception
+    } catch (std::invalid_argument) {
+        std::cout << "You must a number for the celsius!" << std::endl;
+    }
+}
+
+
+int main() {
+    // Displays Program Title
+    std::cout << "Celsius to Fahrenheit Program" << std::endl;
+
+    // Calls fahrenheit() function
+    fahrenheit();
+}
